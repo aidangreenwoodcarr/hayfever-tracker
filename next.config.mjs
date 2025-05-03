932594d936ded02e576d05041e8ccb2f820d9c19
aidangreenwoodcarr26@ <aidangreenwoodcarr26@gmail.com>
2025-05-03 22:32:01 +0100

    ?

--- next.config.mjs
+++ next.config.mjs
@@ -9,25 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Copy the Prisma engine binary to the output directory
-      config.experiments = { ...config.experiments, topLevelAwait: true };
-      
-      // Add a copy plugin to properly bundle the binary
-      config.externals = [...config.externals, 'prisma', '@prisma/client/runtime'];
-    }
-    
-    return config;
-  },
-  
-  // Make sure the Prisma binary is included in the output
-  output: 'standalone',
-  outputFileTracing: true,
-  experimental: {
-    // Optional: If you're using app directory
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  }
 }
 
 export default nextConfig
